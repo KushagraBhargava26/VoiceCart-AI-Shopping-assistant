01bcdd382de337f4a6c39d77c237f7bb1ca5f06c
Kushagra Bhargava <kushagra2112004@gmail.com>
2026-08-24 23:38:09 +0530

    fix: eliminate blank black screen redirect loop by loading VoiceCart Home Dashboard by default

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,13 +40,8 @@ function AppContent() {
 
   useEffect(() => {
     const stored = getStoredUser();
-    const isGuest = sessionStorage.getItem(GUEST_PASSED_KEY);
-
     if (stored) {
       setUser(stored);
-    } else if (!isGuest && location.pathname !== "/login") {
-      // First-time visit: Redirect to /login page by default
-      navigate("/login");
     }
   }, []);
 
